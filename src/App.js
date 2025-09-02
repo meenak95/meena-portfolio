69c6b715889ec30ffdc10a5aae98309c9a89a3e5
anupdavin <davinanup@gmail.com>
2025-09-03 05:58:16 +0800

    Restore full portfolio components and fix configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      <h1>Hello World - Portfolio Test</h1>
+      <p>If you can see this, React is working!</p>
     </div>
   );
 }
